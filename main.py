09ba523efb53130ac79c5508ef4a6c89103218ff
Kayode <drkay4sure@gmail.com>
2024-07-24 21:44:10 +0100

    Done with acivity 1

---
+++ main.py
@@ -0,0 +1,13 @@
+#Initializing products with their values
+ferrari = 420000
+toyota = 21000
+
+#Checking which product has a higher value
+if ferrari > toyota:
+    print("Ferrari is costlier than toyota")
+
+elif toyota > ferrari:
+    print("Toyota is costlier than ferrari")
+
+else:
+    print("Both have the same value")
